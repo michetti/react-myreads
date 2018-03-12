6066d7b27c3a9fcbfc0466f911d37e98dd93a25e
Alexandre Michetti Manduca <a.michetti@gmail.com>
2018-03-11 22:43:23 -0700

    fix: when not in a shelf, show none as selected option

--- src/BookshelfChanger.js
+++ src/BookshelfChanger.js
@@ -28,9 +28,7 @@ class BookshelfChanger extends Component {
     return (
       <div className="book-shelf-changer">
         <select value={shelf} onChange={this.onChange}>
-          <option value="none" disabled>
-            Move to...
-          </option>
+          <option disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
           <option value="read">Read</option>
